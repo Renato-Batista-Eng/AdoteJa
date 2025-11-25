21f913052f17ae8338563c16fbe1c1e0190926c5
agnesdourado <agnes.dourado00@gmail.com>
2025-11-25 19:01:14 -0300

    Exceção personalizada e tratamento de exceções

---
+++ src/exceptions/MinhaExcecao.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class MinhaExcecao extends RuntimeException {
+
+    public MinhaExcecao(String msg) {
+        super(msg);
+    }
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,12 @@ import entities.Adotante;
 import entities.Animal;
 import entities.Endereco;
 import entities.Pet;
+import exceptions.MinhaExcecao;
 import services.Abrigo;
 import services.Adocao;
 import services.AdotanteService;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -35,144 +37,159 @@ public class Main {
                 0. Para sair.
                 """);
 
-        System.out.print("O que deseja fazer? ");
-        int opcao = scan.nextInt();
-        scan.nextLine();
+        int opcao;
+
+        while(true){
+            try {
+                System.out.print("O que deseja fazer? ");
+                opcao = scan.nextInt();
+                scan.nextLine();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Erro capturado -> Você digitou letra onde deveria ser número. Tente novamente!");
+                scan.nextLine();
+            }
+        }
 
         while(opcao != 0) {
 
-            switch (opcao) {
-                case 1:
-                    System.out.print("\nDigite seu nome completo: ");
-                    String adotanteNome = scan.nextLine();
-                    System.out.print("Digite seu cpf: ");
-                    String cpf = scan.next();
-                    System.out.print("Digite seu telefone: ");
-                    String telefone = scan.next();
-                    System.out.println("\n----- Agora preencha os dados do seu endereço ----- \n");
-                    scan.nextLine();
-                    System.out.print("Digite o nome da rua: ");
-                    String rua = scan.nextLine();
-                    System.out.print("Digite o número da casa: ");
-                    int numeroCasa = scan.nextInt();
-                    System.out.print("Digite o nome do bairro: ");
-                    scan.nextLine();
-                    String bairro = scan.nextLine();
-                    System.out.print("Digite o nome da cidade: ");
-                    String cidade = scan.nextLine();
-                    System.out.print("Digite o nome do estado: ");
-                    String estado = scan.nextLine();
-                    endereco = new Endereco(rua, numeroCasa, bairro, cidade, estado);
-                    Adotante adotante = new Adotante(adotanteNome, cpf, telefone, endereco);
-                    adotanteService.registrarAdotante(adotante);
-                    break;
-
-                case 2:
-                    if(adotanteService.getAdotanteList().size() == 0) {
-                        System.out.println("\nNenhum adotante cadastrado até o momento");
+            try {
+                switch (opcao) {
+                    case 1:
+                        System.out.print("\nDigite seu nome completo: ");
+                        String adotanteNome = scan.nextLine();
+                        System.out.print("Digite seu cpf: ");
+                        String cpf = scan.next();
+                        System.out.print("Digite seu telefone: ");
+                        String telefone = scan.next();
+                        System.out.println("\n----- Agora preencha os dados do seu endereço ----- \n");
+                        scan.nextLine();
+                        System.out.print("Digite o nome da rua: ");
+                        String rua = scan.nextLine();
+                        System.out.print("Digite o número da casa: ");
+                        int numeroCasa = scan.nextInt();
+                        System.out.print("Digite o nome do bairro: ");
+                        scan.nextLine();
+                        String bairro = scan.nextLine();
+                        System.out.print("Digite o nome da cidade: ");
+                        String cidade = scan.nextLine();
+                        System.out.print("Digite o nome do estado: ");
+                        String estado = scan.nextLine();
+                        endereco = new Endereco(rua, numeroCasa, bairro, cidade, estado);
+                        Adotante adotante = new Adotante(adotanteNome, cpf, telefone, endereco);
+                        adotanteService.registrarAdotante(adotante);
                         break;
-                    }
 
-                    System.out.println("\nLista de adotantes cadastrados\n--------------------");
-                    for(Adotante a : adotanteService.getAdotanteList()) {
-                        System.out.println(a.exibirInfo());
-                        System.out.println("--------------------------------");
-                    }
-                    break;
-
-                case 3:
-                    System.out.print("\nDigite o nome do pet: ");
-                    String petNome = scan.nextLine();
-                    System.out.print("Digite sua espécie do pet: ");
-                    String petEspecie = scan.nextLine();
-                    System.out.print("Digite a idade do pet em meses: ");
-                    int petIdade = scan.nextInt();
-                    System.out.print("Digite o sexo do pet: ");
-                    String petSexo = scan.next();
-                    Pet pet = new Pet(petNome, petEspecie, petIdade, petSexo, null);
-                    abrigo.registrarPet(pet);
-                    break;
-
-                case 4:
-                    if(abrigo.getPetsDisponiveis().size() == 0) {
-                        System.out.println("\nNenhum pet disponível para adoção no momento");
-                    } else {
-                        System.out.println("\nLista de pets disponíveis para adoção\n--------------------");
-                        for(Pet p : abrigo.getPetsDisponiveis()) {
-                            System.out.println(p.exibirInfo());
-                            System.out.print("--------------------------------");
+                    case 2:
+                        if(adotanteService.getAdotanteList().size() == 0) {
+                            throw new MinhaExcecao("\nErro capturado -> Nenhum adotante cadastrado até o momento");
                         }
-                    }
-                    break;
 
-                case 5:
-                    if(abrigo.getPetsDisponiveis().size() == 0) {
-                        System.out.println("\nNenhum pet disponivel para adoção no momento");
+                        System.out.println("\nLista de adotantes cadastrados\n--------------------");
+                        for(Adotante a : adotanteService.getAdotanteList()) {
+                            System.out.println(a.exibirInfo());
+                            System.out.println("--------------------------------");
+                        }
                         break;
-                    }
 
-                    System.out.print("\nDigite o id do adotante: ");
-                    Long adotanteId = scan.nextLong();
+                    case 3:
+                        System.out.print("\nDigite o nome do pet: ");
+                        String petNome = scan.nextLine();
+                        System.out.print("Digite sua espécie do pet: ");
+                        String petEspecie = scan.nextLine();
+                        System.out.print("Digite a idade do pet em meses: ");
+                        int petIdade = scan.nextInt();
+                        System.out.print("Digite o sexo do pet: ");
+                        String petSexo = scan.next();
+                        Pet pet = new Pet(petNome, petEspecie, petIdade, petSexo, null);
+                        abrigo.registrarPet(pet);
+                        break;
 
-                    System.out.print("Agora digite o id do pet que deseja adotar: ");
-                    Long petId = scan.nextLong();
-
-                    adocao.adotarPet(adotanteId, petId);
-                    break;
-
-                case 6:
-                    if(abrigo.getAnimaisList().size() == 0) {
-                        System.out.println("\nNenhum pet cadastrado no momento");
-                    } else {
-                        System.out.println("\nLista de pets cadastrados no abrigo\n--------------------");
-                        for(Animal animal : abrigo.getAnimaisList()) {
-                            System.out.println(animal.exibirInfo());
-                            System.out.print("--------------------------------");
-                        }
-                    }
-                    break;
-
-                case 7:
-                    if(abrigo.getAnimaisList().size() == 0) {
-                        System.out.println("\nNenhum pet cadastrado no momento");
-                    } else {
-                        abrigo.atualizarPet();
-                    }
-                    break;
-
-                case 8:
-                    if(adotanteService.getAdotanteList().size() == 0) {
-                        System.out.println("\nNenhum adotante cadastrado até o momento");
-                    } else {
-                        adotanteService.atualizarAdotante();
-                    }
-                    break;
-
-                case 9:
-                    if(adotanteService.getAdotanteList().size() == 0) {
-                        System.out.println("\nNenhum adotante cadastrado até o momento");
-                    } else {
-                        System.out.print("Digite o ID do adotante: ");
-                        adotanteId = scan.nextLong();
-
-                        adotante = adotanteService.buscarAdotantePorId(adotanteId);
-                        if(adotante != null) {
-                            if(adotante.getPets().size() != 0) {
-                                System.out.println("Nossos termos de segurança não permite remover um adotante que já tenha adotado um pet.");
-                            } else {
-                                adotanteService.getAdotanteList().remove(adotante);
-                                System.out.println("Adotante removido com sucesso!");
-                            }
+                    case 4:
+                        if(abrigo.getPetsDisponiveis().size() == 0) {
+                            throw new MinhaExcecao("\nErro capturado -> Nenhum pet disponível para adoção no momento");
                         } else {
-                            System.out.println("Adontate não encontrado no banco de dados.");
+                            System.out.println("\nLista de pets disponíveis para adoção\n--------------------");
+                            for(Pet p : abrigo.getPetsDisponiveis()) {
+                                System.out.println(p.exibirInfo());
+                                System.out.print("--------------------------------");
+                            }
                         }
-                    }
-                    break;
+                        break;
 
-                default:
-                    System.out.println("\nOpção inválida. Tente novamente!");
-                    break;
+                    case 5:
+                        if(abrigo.getPetsDisponiveis().size() == 0) {
+                            throw new MinhaExcecao("\nErro capturado -> Nenhum pet disponivel para adoção no momento");
+                        }
 
+                        System.out.print("\nDigite o id do adotante: ");
+                        Long adotanteId = scan.nextLong();
+
+                        System.out.print("Agora digite o id do pet que deseja adotar: ");
+                        Long petId = scan.nextLong();
+
+                        adocao.adotarPet(adotanteId, petId);
+                        break;
+
+                    case 6:
+                        if(abrigo.getAnimaisList().size() == 0) {
+                            throw new MinhaExcecao("\nErro capturado -> Nenhum pet cadastrado no momento");
+                        } else {
+                            System.out.println("\nLista de pets cadastrados no abrigo\n--------------------");
+                            for(Animal animal : abrigo.getAnimaisList()) {
+                                System.out.println(animal.exibirInfo());
+                                System.out.print("--------------------------------\n");
+                            }
+                        }
+                        break;
+
+                    case 7:
+                        if(abrigo.getAnimaisList().size() == 0) {
+                            throw new MinhaExcecao("\nErro capturado -> Nenhum pet cadastrado no momento");
+                        } else {
+                            abrigo.atualizarPet();
+                        }
+                        break;
+
+                    case 8:
+                        if(adotanteService.getAdotanteList().size() == 0) {
+                            throw new MinhaExcecao("\nErro capturado -> Nenhum adotante cadastrado até o momento");
+                        } else {
+                            adotanteService.atualizarAdotante();
+                        }
+                        break;
+
+                    case 9:
+                        if(adotanteService.getAdotanteList().size() == 0) {
+                            throw new MinhaExcecao("\nErro capturado -> Nenhum adotante cadastrado até o momento");
+                        } else {
+                            System.out.print("Digite o ID do adotante: ");
+                            adotanteId = scan.nextLong();
+
+                            adotante = adotanteService.buscarAdotantePorId(adotanteId);
+                            if(adotante != null) {
+                                if(adotante.getPets().size() != 0) {
+                                    throw new MinhaExcecao("Erro capturado -> Nossos termos de segurança não permite remover um adotante que já tenha adotado um pet.");
+                                } else {
+                                    adotanteService.getAdotanteList().remove(adotante);
+                                    System.out.println("Adotante removido com sucesso!");
+                                }
+                            } else {
+                                throw new MinhaExcecao("Erro capturado -> Adontate não encontrado no banco de dados.");
+                            }
+                        }
+                        break;
+
+                    default:
+                        System.out.println("\nOpção inválida. Tente novamente!");
+                        break;
+
+                }
+            } catch (MinhaExcecao e) {
+                System.out.println(e.getMessage());
+            } catch (InputMismatchException e) {
+                System.out.println("Erro capturado -> Você digitou letra onde deveria ser número. Tente novamente!");
+                scan.nextLine();
             }
 
             System.out.println("""
@@ -190,11 +207,21 @@ public class Main {
                 0. Para sair.
                 """);
 
-            System.out.print("O que deseja fazer? ");
-            opcao = scan.nextInt();
-            scan.nextLine();
+            while(true){
+                try {
+                    System.out.print("O que deseja fazer? ");
+                    opcao = scan.nextInt();
+                    scan.nextLine();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Erro capturado -> Você digitou letra onde deveria ser número. Tente novamente!");
+                    scan.nextLine();
+                }
+
+            }
+
         }
-    scan.close();
+
     }
 
 }
